c8feb74514284d68fd893450b01bf6809d1776fb
root <root@security.4linux.com.br>
2019-04-15 15:50:45 +0000

    Primeiro Commit

---
+++ opt-172.16.100.206/maildir.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+for NOME in $(getent passwd | awk -F: '$3 >= 1000 { print $1 }' | egrep -v "nobody|vagrant|suporte")
+
+	echo $NOME Criando diretório
+	maildirmake /home/${NOME}/Maildir
+	maildirmake /home/${NOME}/Maildir/.Enviados
+	maildirmake /home/${NOME}/Maildir/.Rascunhos
+	maildirmake /home/${NOME}/Maildir/.Lixeira
+	maildirmake /home/${NOME}/Maildir/.SPAM
+	echo $NOME Alterando o dono
+	chown ${NOME}: /home/${NOME} -R
+done
